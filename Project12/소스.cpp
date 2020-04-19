a43997edc53159da477c1a215d2bf911c0919dd8
choiwonkoung <48465514+choiwonkoung@users.noreply.github.com>
2020-04-19 17:29:50 +0900

    map을 사용하여 메모리 초과 pair로 바꿔볼 예정 로직은 맞는걸로 생각됨

--- Project12/소스.cpp
+++ Project12/소스.cpp
@@ -8,7 +8,7 @@ typedef struct _Spoly {
 	vector<int> num;
 }Spoly;
 void sum_poly(Spoly &poly, int a, int b) { // 다항식 차수가 a인 항에 계수 b를 더함
-	if (poly.p.find(a) == poly.p.end())poly.p[a] += b;
+	if (poly.p.find(a) != poly.p.end())poly.p[a] += b;
 	else {
 		poly.p[a] = b;
 		poly.num.push_back(a);
@@ -20,13 +20,24 @@ Spoly multi_poly(Spoly& poly1, Spoly& poly2) { // 두 개의 다항식을 곱한후 값을 �
 		for (int j = 0; j < poly2.num.size(); j++) {
 			int x, y;
 			x = poly1.num[i]; // poly1에 존재하는 차수
-			y = poly2.num[i]; // poly2에 존재하는 차수
+			y = poly2.num[j]; // poly2에 존재하는 차수
 			sum_poly(ret, x+y, poly1.p[x]*poly2.p[y]); // 각 항을 곱한다.
 		}
 	}
+	return ret;
 }
 int get_ans(Spoly& poly) {
-	sort(poly.num.begin(), poly.num.end());
+	sort(poly.num.begin(), poly.num.end(),greater<int>());
+	int num=0, ans=0, ret=0;
+	for (int i=0;i<poly.num.size();i++) {
+		int pre = i>0?poly.num[i - 1]:poly.num[i];
+		int now = poly.num[i];
+		ret = max(ret,(pre - now - 1) * num + ans);	
+		ans = (pre - now) * num + poly.p[now] + ans;
+		ret = max(ans, ret);
+		num = num + poly.p[now];
+	}
+	return ret;
 }
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -34,7 +45,7 @@ int main() {
 	int n; cin >> n;
 	for (int i = 0; i < n; i++) {
 		int a,b; cin >> a >> b;
-		sum_poly(poly1, a + b, 1);
+		sum_poly(poly1, b+1, 1);
 		sum_poly(poly1, a, -1);
 	}
 	int m; cin >> m;
@@ -43,6 +54,6 @@ int main() {
 		sum_poly(poly2, a + b, 1);
 		sum_poly(poly2, a, -1);
 	}
-	Spoly ans = multi_poly(poly1, poly2);;
+	Spoly ans = multi_poly(poly1, poly2);
 	cout << get_ans(ans) << endl;
 }
